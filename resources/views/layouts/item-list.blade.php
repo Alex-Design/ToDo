51a01fb806e0ce37a71ce01cad1e0a3ebfbb16ae
Alex Ruddick <20131328+Alex-Design@users.noreply.github.com>
2020-05-16 13:32:40 +0100

    Created timestamp

--- resources/views/layouts/item-list.blade.php
+++ resources/views/layouts/item-list.blade.php
@@ -6,21 +6,25 @@
                     <div class="row">
                         <div class="col-md-9">
                             {{ $todoItem->title }}
+                            <br />
+                            <div class="timestamp">
+                                <i>Created: {{ date('D dS M Y, g:i A', strtotime($todoItem->created_on)) }}</i>
+                            </div>
                         </div>
                         
                         <div class="col-md-3">
                             @if($markAction === 'markComplete')
                                 <div class="complete-todo-item float-right" identifier="todo-item-{{ $todoItem->id }}">
                                     <form method="POST" action="/task/complete?task={{ $todoItem->id }}">
-                                           {{ csrf_field() }}
-                                           <button type="submit" class="btn btn-success">Mark Complete</button>
+                                        {{ csrf_field() }}
+                                        <button type="submit" class="btn btn-success">Mark Complete</button>
                                     </form>
                                 </div>
                             @else
                                 <div class="incomplete-todo-item float-right" identifier="todo-item-{{ $todoItem->id }}">
                                    <form method="POST" action="/task/incomplete?task={{ $todoItem->id }}">
-                                           {{ csrf_field() }}
-                                           <button type="submit" class="btn btn-danger">Mark Incomplete</button>
+                                        {{ csrf_field() }}
+                                        <button type="submit" class="btn btn-danger">Mark Incomplete</button>
                                     </form>
                                 </div>
                             @endif
